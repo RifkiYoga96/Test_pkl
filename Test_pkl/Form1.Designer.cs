3104adf2d2bb92759c5c10b31000ee0884380de7
ThinkPad T14 <ThinkPad T14@DESKTOP-C9FJOHD>
2024-07-02 13:18:54 +0700

    Add project files.

---
+++ Test_pkl/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Test_pkl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.txtnis = new System.Windows.Forms.TextBox();
+            this.btnok = new System.Windows.Forms.Button();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblhasil = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " NIS :";
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(209, 95);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(228, 26);
+            this.txtnama.TabIndex = 2;
+            // 
+            // txtnis
+            // 
+            this.txtnis.Location = new System.Drawing.Point(209, 135);
+            this.txtnis.Name = "txtnis";
+            this.txtnis.Size = new System.Drawing.Size(228, 26);
+            this.txtnis.TabIndex = 3;
+            // 
+            // btnok
+            // 
+            this.btnok.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnok.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnok.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnok.Location = new System.Drawing.Point(209, 215);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(228, 31);
+            this.btnok.TabIndex = 4;
+            this.btnok.Text = "OK";
+            this.btnok.UseVisualStyleBackColor = false;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // datePicker
+            // 
+            this.datePicker.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.datePicker.CustomFormat = "dddd,dd-MM-yyyy";
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datePicker.Location = new System.Drawing.Point(209, 174);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(228, 26);
+            this.datePicker.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tanggal Lahir :";
+            // 
+            // lblhasil
+            // 
+            this.lblhasil.AutoSize = true;
+            this.lblhasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhasil.Location = new System.Drawing.Point(217, 283);
+            this.lblhasil.Name = "lblhasil";
+            this.lblhasil.Size = new System.Drawing.Size(71, 22);
+            this.lblhasil.TabIndex = 7;
+            this.lblhasil.Text = " jsasjaa";
+            this.lblhasil.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(215, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Input Data Diri";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 370);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblhasil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.txtnis);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.TextBox txtnis;
+        private System.Windows.Forms.Button btnok;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblhasil;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Test_pkl/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_pkl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+     
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            string nama = txtnama.Text;
+            string nis = txtnis.Text;
+            if (nama.Length == 0 || nis.Length == 0)
+            {
+                MessageBox.Show("Ada bagian yang belum diisi\nMohon isi terlebih dahulu!","peringatan",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+                
+            }
+            lblhasil.Visible = true;
+            string a = datePicker.Value.ToLongDateString();
+            lblhasil.Text = "Hello "+txtnama.Text+"\n"+txtnis.Text+"\n"+a;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            datePicker.Format = DateTimePickerFormat.Custom;
+            datePicker.CustomFormat = "dddd,dd-MM-yy";
+
+        }
+    }
+}
